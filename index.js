2f5661c1bc4c577107cfb1ac0aee2f7e17d2b458
Maxim Malinousky <maxim.malinouskiy@vaveda.games>
2022-04-14 10:22:01 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const expressHandlebars = require("express-handlebars");
+const bodyParser = require('body-parser');
+
+const cellPhonesRoute = require('./routes/cellphones');
+
+const app = express();
+
+app.engine('handlebars', expressHandlebars.engine());
+app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', cellPhonesRoute);
+
+app.listen(3001, () =>
+{
+    console.log("backend server is running");
+});
